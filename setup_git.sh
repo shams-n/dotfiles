f50f9574b6d1855c00f9081d993bb27a3b21e513
Siang Lim <sianglim@gatech.edu>
2022-06-20 14:29:17 -0700

    Update setup_git.sh

--- setup_git.sh
+++ setup_git.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone https://gist.github.com/a2dad17ac97d440be4d324b0b99dd52f.git .gists
-cp -a .gists/.gitignore ~
-git config credential.helper 'cache --timeout=2419200'
+git clone https://gist.github.com/a2dad17ac97d440be4d324b0b99dd52f.git gists
+cp -a ./gists/.gitconfig ~
+git config --global credential.helper 'cache --timeout=2419200' #one month
